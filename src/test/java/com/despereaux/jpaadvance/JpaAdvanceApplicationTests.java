a6f6d80d2b7ad021e762a421a96dcd09ec64db23
Despereaux <rlaehdwn025@naver.com>
2024-10-14 22:26:23 +0900

    init commit

---
+++ src/test/java/com/despereaux/jpaadvance/JpaAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.despereaux.jpaadvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAdvanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
